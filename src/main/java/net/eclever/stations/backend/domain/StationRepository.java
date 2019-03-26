f1bd7c8912c7fb96bcecbb44cd6ba8524480ffff
Veit Weber <veit@pikodat.com>
2019-03-26 17:22:15 +0100

    Add radius query.

--- src/main/java/net/eclever/stations/backend/domain/StationRepository.java
+++ src/main/java/net/eclever/stations/backend/domain/StationRepository.java
@@ -6,7 +6,6 @@ import com.mongodb.MongoClientURI;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import io.leangen.graphql.annotations.GraphQLArgument;
 import lombok.Data;
 import lombok.extern.java.Log;
 import net.eclever.stations.backend.Environment;
@@ -18,7 +17,8 @@ import java.util.Date;
 import java.util.LinkedList;
 import java.util.function.Consumer;
 
-import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Filters.gte;
+import static com.mongodb.client.model.Filters.or;
 import static com.mongodb.client.model.Sorts.ascending;
 
 /**
@@ -87,13 +87,74 @@ public class StationRepository {
 					.append("$geoWithin", new Document()
 							.append("$box", Arrays.asList(
 									Arrays.asList(
-											boundingBoxNeLatitude,
-											boundingBoxNeLongitude
+											boundingBoxSwLongitude,
+											boundingBoxSwLatitude
 									),
 									Arrays.asList(
-											boundingBoxSwLatitude,
-											boundingBoxSwLongitude
+											boundingBoxNeLongitude,
+											boundingBoxNeLatitude
 									)
+									)
+							)
+					)
+			);
+
+			FindIterable mongoCollection;
+
+			if (first == null || offset == null)
+				mongoCollection = collection
+						.find(query)
+						.sort(ascending("name"));
+			else
+				mongoCollection = collection
+						.find(query)
+						.skip(first)
+						.limit(offset)
+						.sort(ascending("name"));
+
+
+			mongoCollection.forEach((Consumer<Document>) document -> {
+				Station station =
+						new Station(document.get("_id").toString(), document.getString("author"), document.getString("name"), document.getString("operator"),
+								document.get("address") != null ? gson.fromJson(((Document) document.get("address")).toJson(), StationAddress.class) : null,
+								document.get("coordinates") != null ? gson.fromJson(((Document) document.get("coordinates")).toJson(), StationLocation.class) : null,
+								document.get("approach") != null ? gson.fromJson(((Document) document.get("approach")).toJson(), StationLocation.class) : null);
+				cachedStationList.add(station);
+			});
+
+			mongoClient.close();
+			return cachedStationList.toArray(new Station[0]);
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			throw ex;
+		}
+	}
+
+	public Station[] getAllStationsNearby(Double latitude,
+	                                      Double longitude,
+	                                      Double radius,
+	                                      Integer first,
+	                                      Integer offset) {
+		LinkedList<Station> cachedStationList = new LinkedList<>();
+		try {
+			Gson gson = new Gson();
+
+			MongoClientURI uri = new MongoClientURI(System.getenv("STATION_MONGODB_URI"));
+			MongoClient mongoClient = new MongoClient(uri);
+
+			MongoDatabase mongoDatabase = mongoClient.getDatabase(Environment.MongoDbProperties.DB_NAME);
+			MongoCollection collection = mongoDatabase.getCollection(Environment.MongoDbProperties.COLLECTION_NAME);
+
+			Document query = new Document();
+			query.append("coordinates.loc.coordinates", new Document()
+					.append("$geoWithin", new Document()
+							.append("$centerSphere", Arrays.asList(
+									Arrays.asList(
+											longitude,
+											latitude
+											),
+									(radius / 6378.1)
 									)
 							)
 					)
