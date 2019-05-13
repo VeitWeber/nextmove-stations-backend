145de358b0c9098564df982a0fb60af2a3695da3
Veit Weber <veit@pikodat.com>
2019-05-13 16:59:32 +0200

    Fix NEX-1613 wrong SysEnv-Var

--- src/main/java/net/eclever/stations/backend/Environment.java
+++ src/main/java/net/eclever/stations/backend/Environment.java
@@ -1,11 +1,8 @@
 package net.eclever.stations.backend;
 
-/**
- * @author Veit Weber, , $(DATE)
- */
 public interface Environment {
 	interface MongoDbProperties {
-		String DB_NAME = System.getenv("STATION_MONGODB_URI");
+		String DB_NAME = System.getenv("STATION_MONGODB_NAME");
 		String COLLECTION_NAME = "stations";
 	}
 }
