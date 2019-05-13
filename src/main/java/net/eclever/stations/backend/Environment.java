3a977e8287806bf5501a97d5fdf391d90f365fb0
Veit Weber <veit@pikodat.com>
2019-05-13 17:35:39 +0200

    Refactoring

--- src/main/java/net/eclever/stations/backend/Environment.java
+++ src/main/java/net/eclever/stations/backend/Environment.java
@@ -2,6 +2,7 @@ package net.eclever.stations.backend;
 
 public interface Environment {
 	interface MongoDbProperties {
+		String DB_URI = System.getenv("STATION_MONGODB_URI");
 		String DB_NAME = System.getenv("STATION_MONGODB_NAME");
 		String COLLECTION_NAME = "stations";
 	}
